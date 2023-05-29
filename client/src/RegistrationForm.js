07b65a5bf134be0bfcf34c2786674e8f015f0197
vsevolod-viliatser <sevavilyatser@gmail.com>
2023-05-29 19:37:23 +0300

    registration fixed

--- client/src/RegistrationForm.js
+++ client/src/RegistrationForm.js
@@ -58,7 +58,7 @@ const RegistrationForm = () => {
       {error && <p>{error}</p>}
       <form onSubmit={handleSubmit}>
         <div className="form-group">
-          <label htmlFor="username">Username:</label>
+          <label htmlFor="username">Email:</label>
           <input
             type="text"
             className="form-control"
