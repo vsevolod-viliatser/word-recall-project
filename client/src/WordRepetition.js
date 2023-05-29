05eae1e9eccfa29f22d17182fde353c2ac2a9630
vsevolod-viliatser <sevavilyatser@gmail.com>
2023-05-29 21:15:59 +0300

    bug fix

--- client/src/WordRepetition.js
+++ client/src/WordRepetition.js
@@ -1,177 +1,74 @@
-import React, { useEffect, useState } from "react";
-import ProgressBar from "react-bootstrap/ProgressBar";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Card from "./Card";
 
-const WordRepetition = ({ token, onWordsRepeated }) => {
-  const [word, setWord] = useState(null);
-  const [options, setOptions] = useState([]);
-  const [selectedOption, setSelectedOption] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const [hasWordsAvailable, setHasWordsAvailable] = useState(true);
-  const [repetitionCount, setRepetitionCount] = useState(0);
-  const [sessionCompleted, setSessionCompleted] = useState(false);
-  const [incorrectCount, setIncorrectCount] = useState(0);
-  const [progress, setProgress] = useState(0);
-  const [repeatedWords, setRepeatedWords] = useState([]);
+const Display = ({ token, learnedWords }) => {
+  const [selectedWord, setSelectedWord] = useState(null);
+  const [showWords, setShowWords] = useState(true);
 
-  const fetchNextRepetitionWord = async () => {
+  const fetchWordInfo = async (word) => {
     try {
       const response = await fetch(
-        "/api/words/get-repetition-word",
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
+        `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
       );
 
       if (response.ok) {
         const data = await response.json();
-        if (
-          data.message &&
-          data.message === "No words available for repetition"
-        ) {
-          setHasWordsAvailable(false);
-        } else {
-          setWord(data.word);
-          if (Array.isArray(data.options)) {
-            setOptions([...data.options]);
-          } else {
-            console.log("Invalid options data:", data.options);
-            setOptions([]);
-          }
-          setSelectedOption("");
-          setErrorMessage("");
-          setHasWordsAvailable(true);
-        }
+        setSelectedWord(data[0]); // Assuming the API response is an array with a single object
+        setShowWords(false); // Hide the word buttons
       } else {
-        const errorData = await response.json();
-        setErrorMessage(errorData.message);
-        setHasWordsAvailable(false);
+        console.error("Failed to fetch word info");
       }
     } catch (error) {
       console.error(error);
-      setErrorMessage("Network error occurred. Please try again.");
-      setHasWordsAvailable(false);
+      console.error("Network error occurred. Please try again.");
     }
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      if (word) {
-        const response = await fetch(
-          `/api/words/submit-repetition/${word._id}`,
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${token}`,
-            },
-            body: JSON.stringify({ translation: selectedOption }),
-          }
-        );
-
-        if (response.ok) {
-          const data = await response.json();
-          const isTranslationCorrect = data.isCorrect;
-
-          if (isTranslationCorrect) {
-            console.log("Correct translation!");
-            setSelectedOption("");
-            setRepetitionCount((prevCount) => prevCount + 1);
-            setProgress((prevProgress) => prevProgress + (1 / 10) * 100); // Increase progress by 1/15
-            setErrorMessage(""); // Clear the error message
-            setIncorrectCount(0); // Reset the incorrect count
-
-            // Add the repeated word to the repeatedWords array
-            setRepeatedWords((prevWords) => [...prevWords, word]);
-          } else {
-            console.log("Incorrect translation!");
-            setErrorMessage("You're wrong, try again");
-            setIncorrectCount((prevCount) => prevCount + 1);
-          }
-        } else {
-          const errorData = await response.json();
-          console.error(errorData.message);
-        }
-      }
-    } catch (error) {
-      console.error(error);
-    }
+  const closeWordInfo = () => {
+    setSelectedWord(null);
+    setShowWords(true); // Show the word buttons again
   };
 
-  useEffect(() => {
-    if (repetitionCount >= 10) {
-      setSessionCompleted(true);
-      // Pass the repeatedWords array to the parent component
-      onWordsRepeated(repeatedWords);
+  const handleButtonClick = (word) => {
+    if (selectedWord && selectedWord.word === word.english) {
+      // If the card is already open for the clicked word, close it
+      closeWordInfo();
     } else {
-      fetchNextRepetitionWord();
+      // Otherwise, fetch word info for the clicked word
+      fetchWordInfo(word.english);
     }
-  }, [repetitionCount]);
-
-  useEffect(() => {
-    if (incorrectCount === 2) {
-      fetchNextRepetitionWord();
-    }
-  }, [incorrectCount]);
-
-  useEffect(() => {
-    const newProgress = (repetitionCount / 10) * 100;
-    setProgress(newProgress);
-  }, [repetitionCount]);
+  };
 
   return (
-    <div className="container d-flex justify-content-center align-items-center">
-      {hasWordsAvailable ? (
-        <form onSubmit={handleSubmit} className="text-center">
-          <h2 className="mb-4">Repetition</h2>
-          <p className="fs-5">Translate the word:</p>
-          {word && <h3 className="display-4 mb-4">{word.english}</h3>}
-          {errorMessage && <p className="fs-5 text-danger">{errorMessage}</p>}
-          <div>
-            {sessionCompleted ? (
-              <>
-                <p className="fs-5 ">You're good enough for this session!</p>
-                <Link to="/displays">
-                  <button className="btn btn-primary m-2">View Repeated Words</button>
-                </Link>
-              </>
-            ) : (
-              options.map((option, index) => (
-                <button
-                  key={index}
-                  className={`btn btn-lg ${
-                    selectedOption === option
-                      ? "btn-primary selected"
-                      : "btn-light"
-                  }`}
-                  onClick={() => setSelectedOption(option)}
-                >
-                  {option}
-                </button>
-              ))
-            )}
-          </div>
-          <ProgressBar now={progress} label={`${progress.toFixed(0)}%`} />
-          {repeatedWords.length > 0 && (
-            <div>
-              <h5>Repeated Words:</h5>
-              <ul>
-                {repeatedWords.map((word) => (
-                  <li key={word._id}>{word.english}</li>
-                ))}
-              </ul>
+    <div className="container">
+      <h2>Repeated Words</h2>
+      <div className="row justify-content-center">
+        {showWords &&
+          learnedWords &&
+          learnedWords.length > 0 &&
+          learnedWords.map((word) => (
+            <div key={word._id} className="col-md-6">
+              <button
+                className="btn btn-primary m-2 btn-lg rounded-0"
+                onClick={() => handleButtonClick(word)}
+              >
+                {word.english} - {word.ukrainian}
+              </button>
             </div>
-          )}
-        </form>
-      ) : (
-        <p className="fs-5">No words available for repetition</p>
+          ))}
+        {selectedWord && (
+          <Card word={selectedWord} onClose={closeWordInfo} />
+        )}
+      </div>
+      {learnedWords && learnedWords.length === 0 && (
+        <p>No learned words available</p>
       )}
+      <Link to="/word-repetition">
+        <button className="btn btn-primary m-2">Start Repetition</button>
+      </Link>
     </div>
   );
 };
 
-export default WordRepetition;
+export default Display;
